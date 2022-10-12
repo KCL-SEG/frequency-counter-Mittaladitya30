d29f96a019bb206fe0df76bff671c981be9c446a
Aditya Mittal <Aditya.mittal@kcl.ac.uk>
2022-10-12 15:59:13 +0400

    fully working

--- frequencies.py
+++ frequencies.py
@@ -2,6 +2,15 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def frequencies(items):
-    frequencies = {}
-    # Your code goes here
-    return frequencies
+ freq = {}
+
+ for i in items:
+     if isinstance(i, int):
+        items.remove(i)
+        i = str(i)
+        items.append(i)
+ for x in items:
+     freq[x] = items.count(x)
+ print(freq)
+
+frequencies([100, '100', 'hello', 100, '100'])
