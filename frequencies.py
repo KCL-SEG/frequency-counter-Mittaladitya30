171bb6fb4f85a9007956b6c9ffff7eb7a190dbeb
Aditya Mittal <Aditya.mittal@kcl.ac.uk>
2022-12-08 13:42:21 +0000

    final

--- frequencies.py
+++ frequencies.py
@@ -2,19 +2,23 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def frequencies(items):
-  freq = {}
+  frequencies = {}
   count = 0
 
-  for i in items:
-      if isinstance(i, int):
-          items.remove(i)
-          i = str(i)
-          items.append(i)
+  for i in range(len(items)):
+      j = items[0]
+
+      items.remove(j)
+      if isinstance(j, int):
+
+
+          j= str(j)
+      items.append(j)
+
 
   for x in items:
       if x not in freq.keys():
           freq[x] = 1
       else:
           freq[x] += 1
-
-  return freq;
+  return frequencies
